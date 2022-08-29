ba5b41d9bbca21a4cb17656becfeaab150221965
DaggerHashimmoto <DaggerHashimmoto@gmail.com>
2022-08-30 00:21:16 +0500

    //

--- scripts/js.js
+++ scripts/js.js
@@ -1,1 +1,17 @@
-console.log('Yo')
+// console.log('Yo')
+
+const slides = document.querySelectorAll('.slide')
+
+for (const slide of slides) {
+    slide.addEventListener('click', function () {
+        clearActiveClasses()
+
+        slide.classList.add('active')
+    })
+}
+
+function clearActiveClasses() {
+    slides.forEach(function (slide) {
+        slide.classList.remove('active')
+    })
+}
